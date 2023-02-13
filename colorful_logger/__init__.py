51c6795acbbf7234543061ca8f47a8c6de497633
thepoy <thepoy@163.com>
2023-02-13 15:44:57 +0800

    add version.py

--- colorful_logger/__init__.py
+++ colorful_logger/__init__.py
@@ -4,7 +4,7 @@
 # @Email:     thepoy@163.com
 # @File Name: __init__.py
 # @Created:   2021-05-21 13:53:40
-# @Modified:  2023-02-07 13:22:19
+# @Modified:  2023-02-13 15:39:59
 
 from colorful_logger.logger import (
     get_logger,
@@ -12,6 +12,7 @@ from colorful_logger.logger import (
     child_logger,
 )
 from colorful_logger.consts import DEBUG, INFO, WARNING, ERROR, FATAL
+from colorful_logger.version import __version__
 
 __all__ = [
     "logger",
@@ -22,4 +23,5 @@ __all__ = [
     "WARNING",
     "ERROR",
     "FATAL",
+    '__version__'
 ]
