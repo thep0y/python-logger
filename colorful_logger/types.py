943bb820c70eedf3f7503083503e196508beb41e
thepoy <thepoy@163.com>
2023-02-21 09:16:41 +0800

    fix: "TypeError: unsupported operand type(s) for |" where pythonVersion < 3.10

--- colorful_logger/types.py
+++ colorful_logger/types.py
@@ -4,8 +4,9 @@
 # @Email:     thepoy@163.com
 # @File Name: types.py
 # @Created:   2023-02-07 13:08:55
-# @Modified:  2023-02-07 13:09:22
+# @Modified:  2023-02-21 09:14:52
 
 from os import PathLike
+from typing import Union
 
-StrPath = str | PathLike[str]
+StrPath = Union[str, PathLike[str]]
